5f0ef7453b930048bc8db14c0e68534413a7b49a
ylmathfan <1420237+ylmathfan@users.noreply.github.com>
2026-06-07 20:24:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活
 const FILE_PATH = process.env.FILE_PATH || '.tmp';    // 运行目录,sub节点文件保存目录
 const SUB_PATH = process.env.SUB_PATH || 'key';       // 订阅路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http服务订阅端口
-const UUID = process.env.UUID || '0d4a3465-31b0-459f-a539-f18405444e17'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
+const UUID = process.env.UUID || '475fe3ad-cc9f-4c53-ad65-91040a444202'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'galaxy.aiyinmusic.cc.cd';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiM2Q4ZmJjYjRmM2NjODdlN2Q3YTFlMWIwYzM0NjMyNzUiLCJ0IjoiMzFkMTI3MTEtZGNlYi00ZjA2LWEzNmUtZDIwZWYxYWZhODZhIiwicyI6Ik1UQXdZVGczWkRFdFpESTNNQzAwTjJWbExXSmlaamt0T0RSaVpXRXlPV0ZoTlRObSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8061;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'www.visa.cn';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'Galaxy';                  // 节点名称
 
